9195dc02f4ea13f9b9f11a7115c0b10b5b45cb6b
Oskar Köök <oskar@maatriks.ee>
2023-07-16 09:58:17 +0000

    Remove fedora-based build
    On Fedora 38, the patch to allow more than 16 card readers for
    pcsc-lite was removed:
    https://src.fedoraproject.org/rpms/pcsc-lite/c/330bcc20f39656815bf597bf4b305ad3462f9205?branch=f38
    
    This means our distro-based build logic is no longer needed.

--- configure-manifest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-MANIFEST="ee.ria.qdigidoc4.yml"
-
-if [[ "$DISTRO" != "fedora" ]]; then
-  sed -i -z 's/\n      - type: patch\n        path: pcsc\/pcsc-fedora.patch//' $MANIFEST
-fi
